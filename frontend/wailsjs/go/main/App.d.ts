9f02cb554a948a6481a99e288c4c50538e73583e
stephen <8025629+stephenwzm@user.noreply.gitee.com>
2026-07-10 10:19:51 +0800

    Revert "Update"
    This reverts commit d3f73cde62933455e7880887294f7ba5ad30cd09.

--- frontend/wailsjs/go/main/App.d.ts
+++ frontend/wailsjs/go/main/App.d.ts
@@ -3,8 +3,6 @@
 import {subscription} from '../models';
 import {settings} from '../models';
 
-export function AbortSpeedTest():Promise<void>;
-
 export function AddSubscription(arg1:string,arg2:string):Promise<string>;
 
 export function AddSubscriptionWithOptions(arg1:string,arg2:string,arg3:subscription.SubscriptionOptions):Promise<string>;
